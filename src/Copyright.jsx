eea571781703814e584fda13302e1c536510c3b7
ADAidan <ADyer2190@gmail.com>
2024-03-18 06:30:16 -0400

    updated copyright

--- src/Copyright.jsx
+++ src/Copyright.jsx
@@ -4,10 +4,16 @@ import Link from '@mui/material/Link';
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
+    <Typography variant="body2" color="text.secondary" align="center" sx={{
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      p: 1,
+    }}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        Aidan
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
