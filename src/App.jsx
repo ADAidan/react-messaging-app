ddc7cf2c5b9d7060a230a539017cbdc2a4ebbc1a
ADAidan <ADyer2190@gmail.com>
2024-03-18 07:17:22 -0400

    renamed files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import SearchAppBar from './components/appbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Chat from './pages/chat';
+import Messages from './pages/Messages';
 import Profile from './pages/profile';
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
           <SearchAppBar />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/messages' element={<Chat />} />
+            <Route path='/messages' element={<Messages />} />
             <Route path='/profile' element={<Profile />} />
           </Routes>
       </BrowserRouter>
