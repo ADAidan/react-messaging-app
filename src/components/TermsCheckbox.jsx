362d0d903e3a87117768450b443c9cd4f6611c40
ADAidan <ADyer2190@gmail.com>
2024-03-18 22:12:18 -0400

    added EmailNotificationCheckbox and TermsCheckbox components to sign up form

---
+++ src/components/TermsCheckbox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const TermsCheckbox = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <FormControlLabel
+      label="Agree to terms of service"
+      control={<Checkbox
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'Agree to terms of service' }}
+      />} />
+  );
+}
+
+export default TermsCheckbox
