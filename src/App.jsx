e6d7de0c540c4fadd50443d422e1a5d8b079b010
ADAidan <ADyer2190@gmail.com>
2024-03-19 04:08:11 -0400

    refactor to add profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import SearchAppBar from './components/appbar';
+import ResponsiveAppBar from './components/Appbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Messages from './pages/Messages';
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-          <SearchAppBar />
+          <ResponsiveAppBar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/messages' element={<Messages />} />
