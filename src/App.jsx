96ba3b449181f98aa12c1269a88a9a6c6bde03ce
ADAidan <ADyer2190@gmail.com>
2024-03-18 22:36:45 -0400

    removed unused path /profile and component Profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import SearchAppBar from './components/appbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Profile from './pages/profile';
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
           <SearchAppBar />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
           </Routes>
       </BrowserRouter>
     </>
