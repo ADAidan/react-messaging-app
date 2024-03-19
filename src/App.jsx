d1fc826544b2c010652b66dd8c41d410c9d40673
ADAidan <ADyer2190@gmail.com>
2024-03-18 22:20:28 -0400

    added new routes for signup and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import SearchAppBar from './components/appbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Messages from './pages/Messages';
-import Profile from './pages/profile';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
             <Route path='/' element={<Home />} />
             <Route path='/messages' element={<Messages />} />
             <Route path='/profile' element={<Profile />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
           </Routes>
       </BrowserRouter>
     </>
