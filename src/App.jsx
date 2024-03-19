08651bde9de43d41d288fe988f9e6831d469adb8
ADAidan <ADyer2190@gmail.com>
2024-03-19 03:11:27 -0400

    updated component names in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import SearchAppBar from './components/appbar';
+import ResponsiveAppBar from './components/Appbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Messages from './pages/Messages';
-import Profile from './pages/profile';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-          <SearchAppBar />
+          <ResponsiveAppBar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/messages' element={<Messages />} />
-            <Route path='/profile' element={<Profile />} />
           </Routes>
       </BrowserRouter>
     </>
