0adfa36e1753d4cea71ba3d2e40b2c19b4ca5593
ADAidan <ADyer2190@gmail.com>
2024-03-20 18:02:12 -0400

    added search bar

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Toolbar from '@mui/material/Toolbar';
+import SearchBar from '../components/SearchBar';
+import Divider from '@mui/material/Divider';
 
 const Contacts = () => {
   return (
     <Container maxWidth='md'>
-      <h1>Contacts</h1>
+      <Stack direction='column' spacing={2}>
+        <Toolbar/>
+        <SearchBar />
+        <Divider />
+      </Stack>
     </Container>
-  )
-}
+  );
+};
 
 export default Contacts
