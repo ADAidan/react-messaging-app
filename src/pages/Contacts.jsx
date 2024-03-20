d2cb9e62b35fb8d1744987a5edfb38324ff1c4d3
ADAidan <ADyer2190@gmail.com>
2024-03-20 17:54:05 -0400

    added Contacts page

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
+const Contacts = () => {
+  return (
+    <Container maxWidth='md'>
+      <h1>Contacts</h1>
+    </Container>
+  )
+}
+
+export default Contacts
