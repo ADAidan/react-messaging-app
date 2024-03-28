3f2eae75a6e5a6c63be27d0b4edeffea6ec4fd9d
ADAidan <ADyer2190@gmail.com>
2024-03-27 22:23:36 -0400

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
@@ -7,6 +7,19 @@
         "eslint:recommended",
         "plugin:react/recommended"
     ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
@@ -14,11 +27,6 @@
     "plugins": [
         "react"
     ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
     "rules": {
     }
 }
