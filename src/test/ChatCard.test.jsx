ffa27042f6e36f16234efb22e9be45b469eaca47
ADAidan <ADyer2190@gmail.com>
2024-04-02 23:56:58 -0400

    updated Appbar test

--- src/test/ChatCard.test.jsx
+++ src/test/ChatCard.test.jsx
@@ -1,16 +1,8 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
 import ChatCard from "../components/ChatCard";
 
 describe("ChatCard", () => {
-  function setup(jsx) {
-    return {
-      user: userEvent.setup(),
-      ...render(jsx),
-    };
-  }
-
   it("should render the ChatCard component", () => {
     const chat = {
       id: 1,
@@ -22,24 +14,5 @@ describe("ChatCard", () => {
 
     const mottoElement = screen.getByText(/Lorem ipsum dolor sit amet.../i);
     expect(mottoElement).toBeInTheDocument();
-  });
-
-  it("should change the selected chat when clicked", () => {
-    const initialSelectedChat = 1;
-
-    const setSelectedChat = vi.fn();
-
-    const chat = {
-      id: initialSelectedChat,
-      username: "testuser",
-    };
-
-    const { user, ...renderedComponent } = setup(
-      <ChatCard chat={chat} setSelectedChat={setSelectedChat} />,
-    );
-
-    const ChatCardElement = renderedComponent.findByTestId("chat-card");
-    user.click(ChatCardElement);
-    expect(setSelectedChat).toHaveBeenCalledWith(initialSelectedChat);
   });
 });
