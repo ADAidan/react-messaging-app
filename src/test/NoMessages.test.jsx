b373da727c4118c217f38fc3c5ff62c9fdda9278
ADAidan <ADyer2190@gmail.com>
2024-04-03 04:29:12 -0400

    added test for when there are no messages to display

--- src/test/NoMessages.test.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { render } from "@testing-library/react";
-import NoMessages from "../components/NoMessages";
-
-test("renders NoMessages component", () => {
-  const NoMessagesComponent = render(<NoMessages />);
-  expect(NoMessagesComponent).toMatchSnapshot();
-});
