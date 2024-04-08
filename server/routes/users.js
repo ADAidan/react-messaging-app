f9a15ef5f600b4e38b31bd5546669e508190798d
ADAidan <ADyer2190@gmail.com>
2024-04-08 17:43:04 -0400

    removed unnecessary files

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  res.send('respond with list of users');
 });
 
 module.exports = router;
