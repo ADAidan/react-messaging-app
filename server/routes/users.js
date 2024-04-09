b7154fa3073c4b1a3ea3d38a8e80d995a7fbcd5e
ADAidan <ADyer2190@gmail.com>
2024-04-09 02:33:37 -0400

    created user schema

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const User = require('../models/User');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with list of users');
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// POST request to create a new user
+router.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json(user);
 });
 
 module.exports = router;
