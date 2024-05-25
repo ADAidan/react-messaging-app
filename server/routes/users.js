0ade909f7c367c0a641125471b541d1b026cc1f4
ADAidan <ADyer2190@gmail.com>
2024-05-25 17:40:01 -0400

    updated order of endpoints of users resource

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,10 +9,47 @@ router.get('/', async (req, res) => {
   return res.json(users);
 });
 
+// POST request to create a new user
+router.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  return res.json(user);
+});
+
+// POST request to login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  if (user.password !== password) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+
+  return res.json({ message: 'Login successful', id: user.id });
+});
+
+// GET request to get a list of all users
+router.get('/list', async (req, res) => {
+  try {
+    const users = await User.find().select('username profilePicture');
+    return res.json(users);
+  } catch (error) {
+    return res.status(500).send({ message: 'Error finding users', error });
+  }
+});
+
 // GET user settings
 router.get('/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
-  return res.json(user);
+  try {
+    const user = await User.findById(req.params.id);
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).send({ message: 'Error finding user', error });
+  }
 });
 
 // GET user contact -- not used
@@ -132,29 +169,6 @@ router.put('/:userId/send-contact-request', async (req, res) => {
     res.status(500).send({ message: 'Error sending request', error });
   }
   return null;
-});
-
-// POST request to create a new user
-router.post('/signup', async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  return res.json(user);
-});
-
-// POST request to login
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
-  }
-
-  if (user.password !== password) {
-    return res.status(401).json({ message: 'Invalid password' });
-  }
-
-  return res.json({ message: 'Login successful', id: user.id });
 });
 
 module.exports = router;
