950608ba4b14d803cb421efd50e2f8fe1d27f534
ADAidan <ADyer2190@gmail.com>
2024-05-25 22:08:44 -0400

    added Conversation model

---
+++ server/models/Conversation.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+
+const { Schema } = Mongoose;
+
+const ConversationSchema = new Schema({
+  participants: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  messages: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Message',
+  }],
+}, {
+  timestamps: true,
+});
+
+const Conversation = Mongoose.model('Conversation', ConversationSchema);
+
+module.exports = Conversation;
