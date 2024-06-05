d2ad0d18f1ff21a699f425b4d61c0ae66eb08c64
ADAidan <ADyer2190@gmail.com>
2024-06-05 02:22:42 -0400

    added aocketio events to update chat cards

--- src/test/ChatCard.test.jsx
+++ src/test/ChatCard.test.jsx
@@ -5,14 +5,31 @@ import ChatCard from "../components/ChatCard";
 describe("ChatCard", () => {
   it("should render the ChatCard component", () => {
     const chat = {
-      id: 1,
+      _id: 1,
+      contactId: 1,
       username: "testuser",
+      status: "Online",
+      profilePicture: "",
+      messages: [
+        {
+          _id: 1,
+          author: {
+            _id: 1,
+            username: "testuser",
+          },
+          content: "Lorem ipsum dolor sit amet...",
+          createdAt: "2021-09-01T00:00:00.000Z",
+          updatedAt: "2021-09-01T00:00:00.000Z",
+        },
+      ],
     };
     render(<ChatCard chat={chat} />);
     const usernameElement = screen.getByText(/testuser/i);
     expect(usernameElement).toBeInTheDocument();
 
-    const mottoElement = screen.getByText(/Lorem ipsum dolor sit amet.../i);
-    expect(mottoElement).toBeInTheDocument();
+    const lastMessageElement = screen.getByText(
+      /Lorem ipsum dolor sit amet.../i,
+    );
+    expect(lastMessageElement).toBeInTheDocument();
   });
 });
