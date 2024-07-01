2fdacfba296a858967d5c079f365bcbf2f312a6b
ADAidan <ADyer2190@gmail.com>
2024-07-01 19:08:46 -0400

    updated ChatCard to highlight the selected chat

--- server/routes/users.js
+++ server/routes/users.js
@@ -53,7 +53,7 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "10m",
+      expiresIn: "1h",
     });
 
     res.cookie("token", token, {
