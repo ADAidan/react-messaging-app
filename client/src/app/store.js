e6c53dcf74c86b45c685a75af01875a78eac3b21
ADAidan <ADyer2190@gmail.com>
2024-07-04 02:10:50 -0400

    added user data state to redux store and set up persists

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,16 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* eslint-disable import/no-extraneous-dependencies */
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import logger from 'redux-logger';
 import onlineCounterReducer from '../features/onlineCounter/onlineCounterSlice';
 import contactsCounterReducer from '../features/contactsCounter/contactsCounterSlice';
 import pendingCounterReducer from '../features/pendingCounter/pendingCounterSlice';
 import blockedCounterReducer from '../features/blockedCounter/blockedCounterSlice';
 import notificationsCounterReducer from '../features/notificationsCounter/notificationsCounterSlice';
+import userDataReducer from '../features/userData/userDataSlice';
 
-export default configureStore({
-  reducer: {
-    onlineCounter: onlineCounterReducer,
-    contactsCounter: contactsCounterReducer,
-    pendingCounter: pendingCounterReducer,
-    blockedCounter: blockedCounterReducer,
-    notificationsCounter: notificationsCounterReducer,
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  onlineCounter: onlineCounterReducer,
+  contactsCounter: contactsCounterReducer,
+  pendingCounter: pendingCounterReducer,
+  blockedCounter: blockedCounterReducer,
+  notificationsCounter: notificationsCounterReducer,
+  userData: userDataReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    })
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import theme from "./theme";
-import store from "./app/store";
+import { store, persistor } from "./app/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -15,7 +17,9 @@ root.render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
